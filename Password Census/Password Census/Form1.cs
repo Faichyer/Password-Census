7399f676494fb32adcf350af4bd0f120eb0d36cb
Miki Namsrai <miki.namsrai@gmail.com>
2017-05-26 11:31:46 +0200

    Create a text file

---
+++ Password Census/Password Census/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Password_Census
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String path = "C:\\Users\\Miki\\Desktop\\tg.txt";
+            if (!File.Exists(path))
+            {
+                using (StreamWriter sw = File.AppendText(path))
+                {
+                    sw.WriteLine("The very first line!");
+                    sw.Close();
+                }
+            }
+        }
+    }
+}
