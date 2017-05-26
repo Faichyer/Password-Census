ebff9e202f7b31e26e67dce6c9c4e01c5be2bc4e
Miki Namsrai <miki.namsrai@gmail.com>
2017-05-26 11:47:46 +0200

    Read and write the txt file

--- Password Census/Password Census/Form1.cs
+++ Password Census/Password Census/Form1.cs
@@ -26,8 +26,44 @@ namespace Password_Census
                 using (StreamWriter sw = File.AppendText(path))
                 {
                     sw.WriteLine("The very first line!");
+                    sw.WriteLine("From the StreamWriter class");
                     sw.Close();
                 }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            String line;
+            String path = "C:\\Users\\Miki\\Desktop\\tg.txt";
+        try{
+                //Pass the file path and file name to the StreamReader constructor
+                StreamReader sr = new StreamReader(path);
+
+                //Read the first line of text
+                line = sr.ReadLine();
+
+                //Continue to read until you reach end of file
+                while (line != null)
+                {
+                    //write the lie to console window
+                    Console.WriteLine(line);
+                    //Read the next line
+                    line = sr.ReadLine();
+                }
+
+                //close the file
+                sr.Close();
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception: " + ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Executing finally block.");
+               
             }
         }
     }
